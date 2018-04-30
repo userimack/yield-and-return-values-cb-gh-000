6c2737a3157a22a11ce0680bcfc7f654d3effbae
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 17:33:33 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,6 +3,7 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+    binding.pry 
     yield(array[i])
     i += 1
   end
@@ -10,4 +11,4 @@ end
 
 binding.pry 
 
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| s"Hi, #{name}" }
